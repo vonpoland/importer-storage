21d223d955149b73154aa7f60b625002b0786775
marcink <marcinn.krawczyk@gmail.com>
2025-07-18 17:47:02 +0200

    update storage tag

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { S3Storage } from "./logic/storage.js";
+import type { StorageTag } from "./models/storage/tags.js";
 
 export const Storage = {
   S3Storage,
 };
+
+export type { StorageTag };
